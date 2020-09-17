25fe7f19f318750db962976ad874bc89387155e4
talkin24 <35341110+talkin24@users.noreply.github.com>
2020-09-17 17:31:12 +0900

    add 0917 practice

--- 0917_practice/accounts/views.py
+++ 0917_practice/accounts/views.py
@@ -1,22 +1,10 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login as auth_login # 아래 view함수와 이름 겹치지 않도록
-from django.contrib.auth import logout as auth_logout
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, PasswordChangeForm
-from .forms import CustomUserChangeForm
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login as auth_login, logout as auth_logout, update_session_auth_hash
 from django.views.decorators.http import require_POST
-from django.contrib.auth.models import User
-from django.contrib.auth import get_user_model, update_session_auth_hash
+from .forms import CustomUserChangeForm
 
-
-# Create your views here
-def index(request):
-    # User = get_user_model
-    users = User.objects.all()
-    context = {
-        'users': users,
-    }
-    return render(request, 'accounts/index.html', context)
 
 
 def signup(request):
@@ -26,7 +14,7 @@ def signup(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
-            auth_login(request, user) # 회원 가입후 자동 로그인
+            auth_login(request, user)
             return redirect('articles:index')
     else:
         form = UserCreationForm()
@@ -36,13 +24,13 @@ def signup(request):
     return render(request, 'accounts/signup.html', context)
 
 
+
 def login(request):
     if request.user.is_authenticated:
         return redirect('articles:index')
     if request.method == 'POST':
         form = AuthenticationForm(request, request.POST)
         if form.is_valid():
-            # 로그인
             auth_login(request, form.get_user())
             return redirect(request.GET.get('next') or 'articles:index')
     else:
@@ -53,6 +41,7 @@ def login(request):
     return render(request, 'accounts/login.html', context)
 
 
+@require_POST
 def logout(request):
     auth_logout(request)
     return redirect('articles:index')
@@ -61,7 +50,7 @@ def logout(request):
 @login_required
 def update(request):
     if request.method == 'POST':
-        form = CustomUserChangeForm(request.POST, instance=request.user) # 그냥 UserChangeForm은 정보를 너무 많이 보여줌
+        form = CustomUserChangeForm(request.POST, instance=request.user)
         if form.is_valid():
             form.save()
             return redirect('articles:index')
@@ -75,13 +64,11 @@ def update(request):
 
 @login_required
 def change_password(request):
-    if request.user.is_anonymous: # 비회원 처리, 보통은 is_anonymous 보다 is_authenticated 사용
-        return redirect('articles:index')
     if request.method == 'POST':
         form = PasswordChangeForm(request.user, request.POST)
         if form.is_valid():
             user = form.save()
-            update_session_auth_hash(request, user) # 세션 정보를 무효화 하지 않은 상태에서 pw만 업데이트
+            update_session_auth_hash(request, user)
             return redirect('articles:index')
     else:
         form = PasswordChangeForm(request.user)
@@ -95,5 +82,5 @@ def change_password(request):
 def delete(request):
     if request.user.is_authenticated:
         request.user.delete()
-        
+
     return redirect('articles:index')
--- 0917_practice/articles/views.py
+++ 0917_practice/articles/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_http_methods, require_POST
 from .models import Article
 from .forms import ArticleForm
 
-# Create your views here.
+
 def index(request):
     articles = Article.objects.order_by('-pk')
     context = {
@@ -12,11 +11,11 @@ def index(request):
     }
     return render(request, 'articles/index.html', context)
 
-@login_required
+
 @require_http_methods(['GET', 'POST'])
 def create(request):
     if request.method == 'POST':
-        form = ArticleForm(request.POST) 
+        form = ArticleForm(request.POST)
         if form.is_valid():
             article = form.save()
             return redirect('articles:detail', article.pk)
@@ -36,8 +35,7 @@ def detail(request, pk):
     return render(request, 'articles/detail.html', context)
 
 
-@login_required
-@require_http_methods(['GET', 'POST'])
+@require_http_methods(['GET','POST'])
 def update(request, pk):
     article = Article.objects.get(pk=pk)
     if request.method == 'POST':
@@ -53,18 +51,8 @@ def update(request, pk):
     }
     return render(request, 'articles/update.html', context)
 
-
-# @login_required # GET method 요청을 처리할 수 있는 view에서만 사용
-# 문제 발생
-# 1. 비로그인 상태로 POST로 delete
-# 2. login_required로 인해 로그인페이지로 redirect + next 파라미터 가지고
-# 3. 로그인페이지에서 로그인 성공
-# 4. next 파라미터 주소로 redirect됨, 이때 GET으로 처리!
 @require_POST
-# 5. 근데 require_POST로 405 에러 발생
-# 그래서 그냥 require_POST만 이용
 def delete(request, pk):
-    if request.user.is_authenticated:
-        article = Article.objects.get(pk=pk)
-        article.delete()
+    article = Article.objects.get(pk=pk)
+    article.delete()
     return redirect('articles:index')
